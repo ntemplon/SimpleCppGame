24fcda54cac49e90cb1616238eb7d2b65bf2692f
Nathan Templon <ntemplon@gmail.com>
2022-07-31 21:10:53 -0500

    Remove SFML/Network dependency

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,5 @@
 
 #include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
