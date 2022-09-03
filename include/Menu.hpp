b1599a96d15f0da9b0e6839f0976cde80be8f8fb
Nathan Templon <ntemplon@gmail.com>
2022-09-03 15:17:59 -0500

    Resolve silly std::vector const error by changing Menu types to non-const string vectors for now

--- include/Menu.hpp
+++ include/Menu.hpp
@@ -6,11 +6,11 @@
 class Menu
 {
 public:
-    Menu(const std::vector<const std::string> menuItems);
+    Menu(const std::vector<std::string> menuItems);
     Menu();
 
-    const std::vector<const std::string> &getMenuItems() const;
+    const std::vector<std::string> &getMenuItems() const;
 
 private:
-    const std::vector<const std::string> _menuItems;
+    const std::vector<std::string> _menuItems;
 };
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,14 +1,14 @@
 #include <Menu.hpp>
 
-Menu::Menu(const std::vector<const std::string> menuItems) : _menuItems(menuItems)
+Menu::Menu(const std::vector<std::string> menuItems) : _menuItems(menuItems)
 {
 }
 
-Menu::Menu() : Menu(std::vector<const std::string>())
+Menu::Menu() : Menu(std::vector<std::string>())
 {
 }
 
-const std::vector<const std::string> &Menu::getMenuItems() const
+const std::vector<std::string> &Menu::getMenuItems() const
 {
     return this->_menuItems;
 }
